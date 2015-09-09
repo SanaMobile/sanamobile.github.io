dbee63b6b68c49423b96d95f222ee5d1b77fa3c5
ewinkler <winkler.em@gmail.com>
2015-09-09 14:53:37 -0400

    Fixed dropdown menu.

--- static/js/toggle.js
+++ static/js/toggle.js
@@ -38,23 +38,23 @@ $(document).ready(function () {
 		content.toggleClass('open');
 	}
 
-        // Set the side bar toggles
-        var toggleWrappers = $('.toggle-wrapper');
-        // loop over toggles
-        toggleWrappers.each(function(){
-            var wrapper = $(this);
-            // Sets the click function to 
-            $(this).hover(function(event) {
-                //event.preventDefault();
-                var menu = $(this).children('.toggle-menu').first();
-                if (menu.hasClass("height-transition-hidden"))
-                    menu.slideDownTransition();
-                else
-                    menu.slideUpTransition();
-              },function(event) {
-                    var menu = $(this).children('.toggle-menu').first();
-                    menu.slideUpTransition();
-              });
-        });
+  /*
+  * Set the side bar toggles
+  * 
+  */
+  $('.toggle-wrapper').each(function(){ 
+      $(this).hover(function(event){
+        event.preventDefault();
+        $(this).children('.toggle').each(function(event){
+          var toggle = $(this);
+          if (toggle.hasClass("height-transition-hidden"))
+            toggle.slideDownTransition();
+          else
+            toggle.slideUpTransition();
+          },function(event) {
+            toggle.slideUpTransition();
+          })
+      })
+  });
 
 });
